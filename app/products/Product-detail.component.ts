2bb19050751aa9b18e6267fee815dea7bc5275cb
Azadeh <azadeh@yahoo.com>
2017-07-08 16:23:09 -0500

    add nemu with the route & based route is added to product list page

---
+++ app/products/Product-detail.component.ts
@@ -0,0 +1,20 @@
+﻿import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+import { IProduct } from './product';
+
+@Component({
+    templateUrl: 'app/products/product-detail.component.html'
+})
+
+export class ProductDetailComponent implements OnInit {
+    pagetitle: string = "Product Detail";
+    constructor( private _activateRoutr : ActivatedRoute) { }
+
+    ngOnInit(): void {
+        console.log( this._activateRoutr.snapshot.params['id']);
+        let id = +this._activateRoutr.snapshot.params['id'];
+        this.pagetitle += ` : ${id}`;
+    }
+}
+
